ce3e7b166268e6dc266be77b2bd12e8d9ceb4e76
Elvin <e.cyubahiro@alustudent.com>
2024-10-15 21:49:07 +0200

    9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 # Author - Elvin Cyubahiro
 
-
 def print_last_digit(number):
     """Function to print the last digit of a number."""
     last_digit = abs(number) % 10
-    print(last_digit)
+    print("{:02d}".format(last_digit))  # Format to always show two digits
     return last_digit
