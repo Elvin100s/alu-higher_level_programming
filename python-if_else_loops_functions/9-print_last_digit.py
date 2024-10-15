45081f0003090a532a1d3bf1c1952069ab128229
Elvin <e.cyubahiro@alustudent.com>
2024-10-15 21:51:40 +0200

    9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# Author - Elvin Cyubahiro
+print_last_digit = __import__('9-print_last_digit').print_last_digit
 
-def print_last_digit(number):
-    """Function to print the last digit of a number."""
-    last_digit = abs(number) % 10
-    print("{:02d}".format(last_digit))  # Format to always show two digits
-    return last_digit
+print_last_digit(98)      # Expected output: 8
+print_last_digit(0)       # Expected output: 0
+r = print_last_digit(-1024) # Expected output: 4
+print(r)                  # Expected output: 4
