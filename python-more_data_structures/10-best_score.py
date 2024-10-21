789354dd45ace9447881b4319e06d341e57f1300
Elvin <e.cyubahiro@alustudent.com>
2024-10-21 17:04:38 +0200

    10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 
+
 def best_score(a_dictionary):
     """Returns a key with the biggest integer value."""
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+    if not a_dictionary:
         return None
 
-    ret = list(a_dictionary.keys())[0]
-    big = a_dictionary[ret]
-    for k, v in a_dictionary.items():
-        if v > big:
-            big = v
-            ret = k
-    return (ret)
+    # Sort by value and get the last item (highest value)
+    return sorted(a_dictionary.items(), key=lambda item: item[1])[-1][0]
