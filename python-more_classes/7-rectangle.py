e36c766576ff27fdbabad866d0c3fe361a4324e4
Elvin <e.cyubahiro@alustudent.com>
2024-11-08 14:28:32 +0200

    7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -1,14 +1,8 @@
-#!/usr/bin/python3
-
-"""
-This is a module for a class Rectangle
-"""
-
-
 class Rectangle:
     """Class of a Rectangle"""
 
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initialize class"""
@@ -22,13 +16,13 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def width(self, widthValue):
+    def width(self, value):
         """Set width"""
-        if type(widthValue) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if widthValue < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = widthValue
+        self.__width = value
 
     @property
     def height(self):
@@ -36,13 +30,13 @@ class Rectangle:
         return self.__height
 
     @height.setter
-    def height(self, HeightValue):
+    def height(self, value):
         """Set height"""
-        if type(HeightValue) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if HeightValue < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = HeightValue
+        self.__height = value
 
     def area(self):
         """Calculate area"""
@@ -50,35 +44,47 @@ class Rectangle:
 
     def perimeter(self):
         """Calculate perimeter"""
-        width = self.__width
-        height = self.__height
-        if width == 0 or height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return (width + height) * 2
+        return 2 * (self.__width + self.__height)
 
     def __str__(self):
         """Get string representation"""
-        width = self.__width
-        height = self.__height
-        string = ""
-        if width == 0 or height == 0:
-            return string
-        for r in range(height):
-            for c in range(width):
-                string = string + '#'
-            string = string + '\n'
-        return string[:-1]
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return (str(self.print_symbol) * self.__width + "\n") * self.__height[:-1]
 
     def __repr__(self):
-        """Get string."""
-        width = self.__width
-        height = self.__height
-        string = "Rectangle(" + str(width) + \
-            ", " + str(height) + ")"
-        return string
+        """Get string for eval()"""
+        return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
-        """deleted"""
-        if Rectangle.number_of_instances > 0:
-            Rectangle.number_of_instances -= 1
+        """Print message when an instance is deleted"""
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
+# Example usage (from your provided main)
+if __name__ == "__main__":
+    my_rectangle_1 = Rectangle(8, 4)
+    print(my_rectangle_1)
+    print("--")
+    my_rectangle_1.print_symbol = "&"
+    print(my_rectangle_1)
+    print("--")
+
+    my_rectangle_2 = Rectangle(2, 1)
+    print(my_rectangle_2)
+    print("--")
+    Rectangle.print_symbol = "C"
+    print(my_rectangle_2)
+    print("--")
+
+    my_rectangle_3 = Rectangle(7, 3)
+    print(my_rectangle_3)
+
+    print("--")
+
+    my_rectangle_3.print_symbol = ["C", "is", "fun!"]
+    print(my_rectangle_3)
+
+    print("--")
