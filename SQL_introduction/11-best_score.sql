c354c643e969d1e2730a86fe615da10b91261626
Elvin <e.cyubahiro@alustudent.com>
2024-11-14 17:39:34 +0200

    11

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,2 @@
--- Update Bob's score to 10 in the second_table
-UPDATE second_table
-SET score = 10
-WHERE name = 'Bob';
+-- List records where score is greater than or equals 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
