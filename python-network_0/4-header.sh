c40e43329589757fce21d345e81a6510d80d7b79
Elvin <e.cyubahiro@alustudent.com>
2024-11-18 14:50:15 +0200

    4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 # Sends request with an added custom header
-
-# Check if a URL argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send the GET request with the custom header and display the response body
 curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
