338542732c680924cdbcea116d2e58929fa08459
Elvin <e.cyubahiro@alustudent.com>
2024-11-19 12:27:09 +0200

    Update 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -6,11 +6,15 @@ This script fetches the status from a given URL using the requests library.
 import requests
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"  # Updated URL
-    response = requests.get(url)
-    
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    url = "https://alu-intranet.hbtn.io/status"  # Correct URL
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad responses (4xx and 5xx)
+
+        print("Body response:")
+        print("\t- type: {}".format(type(response.text)))
+        print("\t- content: {}".format(response.text))
+    except requests.exceptions.RequestException as e:
+        print("Error fetching the URL: {}".format(e))
 
 
