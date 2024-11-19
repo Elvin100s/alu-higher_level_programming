a7f9fa4237d4e7b1726a3bfb28a81f69016b7977
Elvin <e.cyubahiro@alustudent.com>
2024-11-19 12:39:18 +0200

    0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,11 +6,12 @@ This script fetches the status from a given URL using the urllib library.
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as request:
+    url = "https://alu-intranet.hbtn.io/status"  # URL to fetch
+    with urllib.request.urlopen(url) as response:
+        body = response.read()  # Read the response body
         print("Body response:")
-        data = request.read()
-        print("\t- type: {}".format(type(data)))
-        print("\t- content: {}".format(data))
-        print("\t- utf8 content: {}".format(data.decode("utf-8")))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
         
