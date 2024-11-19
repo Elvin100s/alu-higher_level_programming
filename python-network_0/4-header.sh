ad2e1bc56163ec97a7549222ad8a9cffc1f7ad6d
Elvin <e.cyubahiro@alustudent.com>
2024-11-19 15:03:40 +0200

    4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to a URL with a custom header.
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
-echo "OK"
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1" && echo "OK"
