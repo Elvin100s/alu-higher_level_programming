6393a04cc85448b5bd0865a546c3486fea5156f8
Elvin <e.cyubahiro@alustudent.com>
2024-11-19 12:46:40 +0200

    0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-'''
-Script that fetches https://intranet.hbtn.io/status and http://0.0.0.0:5050/status
-'''
+"""
+This script fetches the status from the URL https://alu-intranet.hbtn.io/status
+and displays the response body in a formatted manner.
+"""
 
 import urllib.request as req
 
 def fetch_status(url):
+    """
+    Fetches the status from the given URL and prints the response details.
+
+    Args:
+        url (str): The URL to fetch the status from.
+    """
     with req.urlopen(req.Request(url, headers={'User-Agent': 'Mozilla/5.0'})) as res:
         content = res.read()
         print("Body response:")
@@ -13,9 +20,5 @@ def fetch_status(url):
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
 
-if __name__ == '__main__':
-    # Fetching from the official URL
-    fetch_status('https://intranet.hbtn.io/status')
-    
-    # Fetching from the local server (make sure it's running)
-    fetch_status('http://0.0.0.0:5050/status')
+if __name__ == "__main__":
+    fetch_status('https://alu-intranet.hbtn.io/status')
