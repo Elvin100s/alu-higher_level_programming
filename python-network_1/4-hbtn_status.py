2c053b908fe26670d589b1a611469df82d6bf60d
Elvin <e.cyubahiro@alustudent.com>
2024-11-19 12:22:21 +0200

    4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""
+    This script makes a request and displays a
+    response using the requests package.
+"""
 
-"""
-This script makes a displays a response using the requests package.
-"""
 import requests
 
 if __name__ == "__main__":
