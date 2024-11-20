b372bd432758f8a067a283a35a196ac9bcde49a7
Elvin <e.cyubahiro@alustudent.com>
2024-11-20 14:06:32 +0200

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""
-This script fetches the status from the URL https://alu-intranet.hbtn.io/status
-and displays the response body in a formatted manner.
-"""
+"""  fetches https://alu-intranet.hbtn.io/status  """
+import urllib.request
 
-import urllib.request as req
-
-def fetch_status(url):
-    """
-    Fetches the status from the given URL and prints the response details.
-
-    Args:
-        url (str): The URL to fetch the status from.
-    """
-    with req.urlopen(req.Request(url, headers={'User-Agent': 'Mozilla/5.0'})) as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = "https://alu-intranet.hbtn.io/status"
 
 if __name__ == "__main__":
-    fetch_status('https://alu-intranet.hbtn.io/status')
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode("utf-8"))
